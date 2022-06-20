5639b6be2bcc09ea09761ee4ac144bb06ad75018
zxcmnb102 <zxcmnb05@naver.com>
2022-06-20 14:01:44 +0900

    refactor: database code

--- app/src/main/java/com/example/government25/data/database/dao/PostDao.kt
+++ app/src/main/java/com/example/government25/data/database/dao/PostDao.kt
@@ -6,15 +6,16 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.government25.data.database.entity.PostEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PostDao {
 
     @Query("SELECT * FROM post_data")
-    fun getAll(): LiveData<List<PostEntity>>
+    fun getAll(): Flow<List<PostEntity>>
 
     @Query("SELECT * FROM post_data WHERE postIdx = :idx")
-    fun getPostByIdx(idx: Int): PostEntity
+    suspend fun getPostByIdx(idx: Int): PostEntity
 
     @Delete
     suspend fun delete(post: PostEntity)
--- app/src/main/java/com/example/government25/data/repository/PostRepository.kt
+++ app/src/main/java/com/example/government25/data/repository/PostRepository.kt
@@ -1,23 +1,23 @@
 package com.example.government25.data.repository
 
-import androidx.lifecycle.LiveData
 import com.example.government25.data.database.dao.PostDao
 import com.example.government25.data.database.entity.PostEntity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class PostRepository @Inject constructor(private val postDao: PostDao) {
 
-    val getAllPost: LiveData<List<PostEntity>> = postDao.getAll()
+    fun getAllPost(): Flow<List<PostEntity>> = postDao.getAll().flowOn(Dispatchers.IO).conflate()
 
-    suspend fun getPostByIdx(idx: Int) {
-        postDao.getPostByIdx(idx)
-    }
+    suspend fun getPostByIdx(idx: Int) = postDao.getPostByIdx(idx)
 
-    suspend fun addTodo(post: PostEntity) {
-        postDao.insert(post)
-    }
 
-    suspend fun deletePost(post: PostEntity) {
-        postDao.delete(post)
-    }
+    suspend fun addTodo(post: PostEntity) = postDao.insert(post)
+
+
+    suspend fun deletePost(post: PostEntity) = postDao.delete(post)
+
 }
